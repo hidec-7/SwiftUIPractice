0e8521f96faf1d8d4d780d80c42919f4773814a7
hidec-7 <hideto.dev7@gmail.com>
2024-11-11 00:59:05 +0900

    first commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by h-chaya on 2024/11/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
